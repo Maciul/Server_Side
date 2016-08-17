cc8ff9cd7f1353eb3199d6e34b55c42dc7f08016
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-08-17 11:18:35 -0600

    add start of weather api call

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({
-    message: 'We are in great success!' 
+    message: 'We are in great success!'
+  });
+});
+
+router.get('/weather', function(request, response, next) {
+  request("http://api.aerisapi.com/forecasts/80211?client_id=" +
+  process.env.CLIENT_ID + "&client_secret=" + process.env.CLIENT_SECRET, function(err, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    }
   });
 });
 
